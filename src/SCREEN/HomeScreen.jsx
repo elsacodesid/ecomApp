e0612302449b23fc05ed84d0d4b5c5db5ecfc042
elsacodesid <elsacodesid@gmail.com>
2024-07-01 13:57:48 +0700

    update

--- src/SCREEN/HomeScreen.jsx
+++ src/SCREEN/HomeScreen.jsx
@@ -1,14 +1,22 @@
-import {StyleSheet, Text, View} from 'react-native';
+import {StyleSheet, Text, TextInput, View} from 'react-native';
 import React from 'react';
 import Icon from 'react-native-vector-icons/FontAwesome';
 import AntDesign from 'react-native-vector-icons/AntDesign';
 import LinearGradient from 'react-native-linear-gradient';
 import Header from '../components/Header';
+import Fontisto from 'react-native-vector-icons/Fontisto';
 
 const HomeScreen = () => {
   return (
     <LinearGradient colors={['#FDF0F3', '#FFFBFC']} style={styles.container}>
       <Header />
+      <Text style={styles.matchText}>Match Your Style</Text>
+      <View style={styles.inputContainer}>
+        <View style={styles.iconContainer}>
+          <Fontisto name={'search'} size={26} color={'#c0c0c0'} />
+        </View>
+        <TextInput style={styles.textInput}></TextInput>
+      </View>
     </LinearGradient>
   );
 };
@@ -18,5 +26,24 @@ export default HomeScreen;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+  },
+  matchText: {
+    fontSize: 28,
+    color: '#000000',
+    marginTop: 25,
+  },
+  inputContainer: {
+    backgroundColor: '#ffffff',
+    height: 48,
+    borderRadius: 12,
+    alignItems: 'center',
+    flexDirection: 'row',
+  },
+  iconContainer: {
+    marginHorizontal: 20,
+  },
+  textInput: {
+    flex: 1,
+
   },
 });
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,43 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import {Image, StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+import {isNativePlatformSupported} from 'react-native-screens/lib/typescript/core';
 
 const Header = () => {
   return (
-    <View>
-      <Text>Header</Text>
+    <View style={styles.container}>
+      <View style={styles.menuContainer}>
+        <Image source={require('../assets/menu.png')} style={styles.menu} />
+   
+      </View>
+      <Image source={require('../assets/pfp.png')} style={styles.pfp} />
     </View>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: "center",
+    padding: 20
+  },
+  menuContainer: {
+    backgroundColor: '#ffffff',
+    height: 44,
+    width: 44,
+    borderRadius: 22,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  menu: {
+    height: 20,
+    weight: 20,
+  },
+  pfp: {
+    height: 44,
+    width: 44,
+    borderRadius: 22,
+  },
+});
